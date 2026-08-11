9cbf0b395408e3e4206ed27e96d5e98eb3eb72f7
Shivraj Desale <shivrajdesale@gmail.com>
2026-08-11 16:55:36 +0530

    File storage path and additional properties

--- file-storage/src/main/java/com/shivraj/filestorage/service/StorageService.java
+++ file-storage/src/main/java/com/shivraj/filestorage/service/StorageService.java
@@ -4,6 +4,7 @@ import com.shivraj.filestorage.exc.GenericErrorResponse;
 import com.shivraj.filestorage.model.File;
 import com.shivraj.filestorage.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,27 +12,28 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class StorageService {
     private final FileRepository fileRepository;
+
+    @Value("${file.storage.path}")
     private String FOLDER_PATH;
 
     @PostConstruct
     public void init() {
-        String currentWorkingDirectory = System.getProperty("user.dir");
-
-        FOLDER_PATH = currentWorkingDirectory + "/file-storage/src/main/resources/attachments";
-
+       
         java.io.File targetFolder = new java.io.File(FOLDER_PATH);
 
         if (!targetFolder.exists()) {
             boolean directoriesCreated = targetFolder.mkdirs();
             if (!directoriesCreated) {
                 throw GenericErrorResponse.builder()
-                        .message("Unable to create directories")
+                        .message("Unable to create directories at: " +FOLDER_PATH)
                         .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
                         .build();
             }
@@ -40,10 +42,10 @@ public class StorageService {
 
     public String uploadImageToFileSystem(MultipartFile file) {
         String uuid = UUID.randomUUID().toString();
-        String filePath = FOLDER_PATH + "/" + uuid;
+        Path filePath = Paths.get(FOLDER_PATH, uuid);
 
         try {
-            file.transferTo(new java.io.File(filePath));
+            file.transferTo(filePath.toFile());
         } catch (IOException e) {
             throw GenericErrorResponse.builder()
                     .message("Unable to save file to storage")
@@ -54,14 +56,13 @@ public class StorageService {
         fileRepository.save(File.builder()
                 .id(uuid)
                 .type(file.getContentType())
-                .filePath(filePath).build());
+                .filePath(filePath.toString()).build());
         return uuid;
     }
 
     public byte[] downloadImageFromFileSystem(String id) {
         try {
-            return Files.readAllBytes(new java.io.File(findFileById(id)
-                    .getFilePath()).toPath());
+            return Files.readAllBytes(Paths.get(findFileById(id).getFilePath()));
         } catch (IOException e) {
             throw GenericErrorResponse.builder()
                     .message("Unable to read file from storage")
