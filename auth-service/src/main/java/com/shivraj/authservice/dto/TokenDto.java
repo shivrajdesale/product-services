9b2292e54fb4984174e61553fd9ba1eea246c82b
Shivraj Desale <shivrajdesale@gmail.com>
2026-08-11 20:23:39 +0530

    maven based auth-service to process

---
+++ auth-service/src/main/java/com/shivraj/authservice/dto/TokenDto.java
@@ -0,0 +1,10 @@
+package com.shivraj.authservice.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TokenDto {
+    private String token;
+}
---
+++ auth-service/src/main/java/com/shivraj/authservice/enums/Role.java
@@ -0,0 +1,5 @@
+package com.shivraj.authservice.enums;
+
+public enum Role {
+    ADMIN, USER
+}
